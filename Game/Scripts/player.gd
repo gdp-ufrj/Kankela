53291369355b6cb80a5c651ab4b7720579c8726f
JohnG077 <joaoglmarques07@gmail.com>
2025-04-12 14:22:29 -0300

    Add: diálogos
    Foi adicionado o plugin Dialogue Manager e alguns diálogos no início do jogo

--- Game/Scripts/player.gd
+++ Game/Scripts/player.gd
@@ -4,36 +4,50 @@ extends CharacterBody2D
 const SPEED = 300.0
 
 @onready var animacao := $Anim as AnimatedSprite2D
+@onready var cutscene_mode
 
+func _ready():
+	start_cutscene(preload("res://Dialogues/Start.dialogue"))
+
+func _process(delta: float) -> void:
+	pass
 
 func _physics_process(delta: float) -> void:
-	# Movimento horizontal (eixo X)
-	var direction_x := Input.get_axis("ui_left", "ui_right")
-	var direction_y := Input.get_axis("ui_up", "ui_down")
-	
-	# Combina as direções em um vetor e normaliza para movimento diagonal
-	var direction := Vector2(direction_x, direction_y).normalized()
-	
-	if direction != Vector2.ZERO:
-		velocity.x = direction.x * SPEED
-		velocity.y = direction.y * SPEED
+	if !cutscene_mode:
+		# Movimentação no eixo X e no eixo Y  
+		var direction_x := Input.get_axis("ui_left", "ui_right")
+		var direction_y := Input.get_axis("ui_up", "ui_down")
 		
-		# Animação de andar
-		$Anim.play("Andando")
+		# Combina as direções em um vetor e normaliza para movimento diagonal
+		var direction := Vector2(direction_x, direction_y).normalized()
 		
-		# Vira o sprite horizontalmente conforme a direção
-		if direction.x > 0:
-			animacao.scale.x = 1
-		elif direction.x < 0:
-			animacao.scale.x = -1
+		if direction != Vector2.ZERO:
+			velocity.x = direction.x * SPEED
+			velocity.y = direction.y * SPEED
 			
-	else:
-		# Desacelera suavemente até parar
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.y = move_toward(velocity.y, 0, SPEED)
-		
-		# Animação de idle (parado)
-		$Anim.play("Parada")
+			# Animação de andar
+			$Anim.play("Andando")
+			
+			# Vira o sprite horizontalmente conforme a direção
+			if direction.x > 0:
+				animacao.scale.x = 1
+			elif direction.x < 0:
+				animacao.scale.x = -1
+				
+		else:
+			# Desacelera suavemente até parar
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			velocity.y = move_toward(velocity.y, 0, SPEED)
+			
+			# Animação de idle (parado)
+			$Anim.play("Parada")
 
-	# Aplicando o movimento
-	move_and_slide()
+		# Aplicando o movimento
+		move_and_slide()
+
+
+func start_cutscene(dialogue_resource: DialogueResource) -> void:
+	cutscene_mode = true
+	DialogueManager.show_dialogue_balloon(dialogue_resource)
+	await DialogueManager.dialogue_ended
+	cutscene_mode = false
