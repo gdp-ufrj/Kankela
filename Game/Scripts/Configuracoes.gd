d4fdb4382e24158821985c9cdc0a305096b70a5b
Angelo <mangelogm28@gmail.com>
2025-07-21 15:18:49 -0300

    fix: Ajusta exibição do jogo para funcionar com o sistema de configurações e resolve bug da menor resolução Quando a resolução padrão era 1152x648, selecionar a resolução 640x360 não alterava pra ela. Coloquei 640x360 como resolução padrão e agora tudo funciona :)

--- Game/Scripts/Configuracoes.gd
+++ Game/Scripts/Configuracoes.gd
@@ -4,7 +4,7 @@ extends Control
 @onready var borderless = get_node("Panel/menu_config/HBoxContainer/SemBordas")
 @onready var fullscreen = get_node("Panel/menu_settings/HBoxContainer/TelaCheia")
 @onready var resolution = get_node("Panel/menu_config/tipo_resolucoes")
-@onready var screen_size = DisplayServer.screen_get_size()	# Tamanho da tela atual
+@onready var screen_size = DisplayServer.screen_get_size() # Tamanho da tela atual
 @onready var window_menu = get_node("Panel/menu_config/HBoxContainer")
 @onready var game_viewport = get_tree().root.find_child("GameViewport", true, false)
 
@@ -23,11 +23,10 @@ func save_settings():
 	GlobalSettings.window_size = resolution.get_selected_id()
 
 
-
 func _ready():
 	load_settings()
 	# Define um tamanho mínimo pra janela do jogo
-	DisplayServer.window_set_min_size(Vector2i(1152,648))
+	DisplayServer.window_set_min_size(Vector2i(640, 360))
 	
 
 # Definindo a resolução do jogo
@@ -35,16 +34,16 @@ func _on_resolutions_item_selected(index: int) -> void:
 	match index:
 		0:
 			DisplayServer.window_set_size(Vector2i(1920, 1080))
-			change_viewport(1920,1080)
+			change_viewport(1920, 1080)
 		1:
 			DisplayServer.window_set_size(Vector2i(1600, 900))
-			change_viewport(1600,900)
+			change_viewport(1600, 900)
 		2:
 			DisplayServer.window_set_size(Vector2i(1280, 720))
-			change_viewport(1280,720)
+			change_viewport(1280, 720)
 		3:
 			DisplayServer.window_set_size(Vector2i(1152, 648))
-			change_viewport(1152,648)
+			change_viewport(1152, 648)
 		4:
 			DisplayServer.window_set_size(Vector2i(640, 360))
 			change_viewport(640, 360)
@@ -52,20 +51,20 @@ func _on_resolutions_item_selected(index: int) -> void:
 
 func change_viewport(size_x, size_y):
 	if game_viewport:
-		game_viewport.set_size(Vector2(size_x, size_y))  # ou outro tamanho desejado
+		game_viewport.set_size(Vector2(size_x, size_y)) # ou outro tamanho desejado
 	else:
 		print("GameViewport não encontrado!")
 
 # Centraliza a janela
 func center_window():
-	var window_size = DisplayServer.window_get_size()	# Tamanho atual da janela
-	var centered_position = (screen_size - window_size) / 2	# Calcula a posição central da tela baseado no tamanho da tela e da janela
-	DisplayServer.window_set_position(centered_position)	# Centraliza a posição da janela
+	var window_size = DisplayServer.window_get_size() # Tamanho atual da janela
+	var centered_position = (screen_size - window_size) / 2 # Calcula a posição central da tela baseado no tamanho da tela e da janela
+	DisplayServer.window_set_position(centered_position) # Centraliza a posição da janela
 
 
 # Definindo se será no modo janela ou tela cheia ou sem bordas
 func change_checkboxes(node):
-	for i in range(window_menu.get_child_count()):		# Pega a quantidade de filhos do nó pai (HBoxContainer)
+	for i in range(window_menu.get_child_count()): # Pega a quantidade de filhos do nó pai (HBoxContainer)
 		if node == window_menu.get_child(i):
 			window_menu.get_child(i).disabled = true
 			GlobalSettings.window_type = i
@@ -99,7 +98,6 @@ func _on_full_screen_pressed() -> void:
 	change_checkboxes(fullscreen)
 
 
-
 func apply_window_mode() -> void:
 	match resolution.get_selected_id():
 		0:
@@ -110,6 +108,8 @@ func apply_window_mode() -> void:
 			DisplayServer.window_set_size(Vector2i(1280, 720))
 		3:
 			DisplayServer.window_set_size(Vector2i(1152, 648))
+		4:
+			DisplayServer.window_set_size(Vector2i(640, 360))
 	center_window()
 
 
--- Game/Scripts/GlobalSettings.gd
+++ Game/Scripts/GlobalSettings.gd
@@ -2,6 +2,6 @@ extends Node
 
 
 # Dados da configuração
-@onready var is_windowed : bool = true	# Define se está em modo janela
-@onready var window_size : int = 3			# Define o tipo de tamanho da janela
-@onready var window_type : int = 0		# Define o modo de janela (windowed, borderless, fullscreen)
+@onready var is_windowed: bool = true # Define se está em modo janela
+@onready var window_size: int = 4 # Define o tipo de tamanho da janela
+@onready var window_type: int = 0 # Define o modo de janela (windowed, borderless, fullscreen)
